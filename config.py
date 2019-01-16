75396729f9e6cf7d1cf2335e611927bce2e8f907
Nate-Wessel <bike756@gmail.com>
2019-01-16 10:22:19 -0500

    doc cleanup

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # global variables for this script
-from pytz import timezone
-import datetime as dt
 
 # timezone of all data
+from pytz import timezone
 tz = timezone('America/Toronto')
 
 # where the data from OTP is
 input_dir = '/home/nate/dissdata/routing/'
 
-# define time window to clip to 
+# define time window to clip to
+import datetime as dt 
 start = dt.time(6,30,0) # h,m,s; 6:30am
 end = dt.time(22,0,0) # h,m,s;  10:00pm
 
-walk_speed = 1.34112 # 3mph in meters per second 
 # (http://dev.opentripplanner.org/apidoc/1.0.0/resource_PlannerResource.html)
+walk_speed = 1.34112 # 3mph in meters per second 
