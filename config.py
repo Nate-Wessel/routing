a080188b32c40d9382b23c89fdd15a79ee80d02c
Nate-Wessel <bike756@gmail.com>
2019-01-18 16:00:41 -0500

    changed window parameter

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ tz = timezone('America/Toronto')
 # where the data from OTP is
 input_dir = '/home/nate/dissdata/routing/'
 
-# define time window to clip to
+# define time window to clip travel to
 import datetime as dt 
-window_start_time = dt.time(6,30,0) # h,m,s; 6:30am
-window_end_time   = dt.time(22,0,0) # h,m,s; 10:00pm
+window_start_time = dt.time(6,0,0) # h,m,s 
+window_end_time   = dt.time(9,0,0) # h,m,s
 
 # (http://dev.opentripplanner.org/apidoc/1.0.0/resource_PlannerResource.html)
 walk_speed = 1.34112 # 3mph in meters per second 
