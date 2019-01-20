515547bc198fafcc25530f2e8adff0ceaf35c53f
Nate-Wessel <bike756@gmail.com>
2019-01-20 17:54:44 -0500

    add access scores to DB od table

---
+++ etc/add-access-score-to-table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE temp (
+	orig_id smallint,
+	dest_id smallint,
+	a numeric
+);
+COPY temp FROM '/home/nate/routing/data/test-access.csv' CSV HEADER;
+
+ALTER TABLE ttc_od ADD COLUMN access_12_negexp_45 numeric;
+UPDATE ttc_od SET access_12_negexp_45 = a 
+FROM temp WHERE dest_id = uid;
+DROP TABLE temp;
