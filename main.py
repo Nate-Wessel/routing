13b0a8fe7d2411171aca7bcc4efb770fc150edbe
Nate-Wessel <bike756@gmail.com>
2019-02-06 12:37:13 -0500

    adding extra attributes to OD output file

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-# tentative analysis script for routing paper
+# this script iterates over a selection of OD pairs producing
+#	1) OD level stats
+#	2) OD-departure-time level departures for the three route methods
 
-from OD import OD
 import csv
+from OD import OD
 
-# read in a list of prespecified pairs
-ODs = []
-with open('data/1k_od_sample.csv') as f:
-	reader = csv.DictReader(f)
-	for r in reader:
-		ODs.append( OD( r['o'], r['d'] ) )
-
-# write out a summary file
+# open a file for writing output
 with open('data/summary.csv','w+') as f:
 	fieldnames = [
-		'oid','did',
+		'i','o','d','azimuth','arc',
 		'sched_ent','retro_ent',
 		'sched_it_n','retro_it_n' ]
 	writer = csv.DictWriter(f,fieldnames=fieldnames)
 	writer.writeheader()
-	for od in ODs:
-		data_dict = {
-			'oid':od.orig,
-			'did':od.dest,
-			'sched_ent':od.sched_entropy,
-			'retro_ent':od.retro_entropy,
-			'sched_it_n':len(od.sched_itins),
-			'retro_it_n':len(od.retro_itins)
-		}
-		writer.writerow(data_dict)
+	# open a file for reading input
+	with open('data/1k_od_sample.csv') as f:
+		reader = csv.DictReader(f)
+		for r in reader:
+			# construct the OD
+			od = OD( r['o'], r['d'] )
+			# add attributes to output file
+			data_dict = {
+				'i':r['i'], 'o':od.orig, 'd':od.dest,
+				'azimuth':r['azimuth'], 'arc':r['arc'],
+				'sched_ent':od.sched_entropy,
+				'retro_ent':od.retro_entropy,
+				'sched_it_n':len(od.alter_itins('sched')),
+				'retro_it_n':len(od.alter_itins('retro'))
+			}
+			writer.writerow(data_dict)
