ae31b47888ec9feacb8e9657ed54d1d76726e8c7
Nate-Wessel <bike756@gmail.com>
2019-02-12 10:58:05 -0500

    added area-based weight field to sampled OD pairs

---
+++ etc/add-azimuth-and-distance-to-OD-pairs.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE sampled_ods (
+	i int,
+	o smallint,
+	d smallint,
+	azimuth real,
+	arc real,
+	weight real -- origin area weight X destination area weight
+);
+COPY sampled_ods (i,o,d) FROM '/home/nate/routing/data/all_sampled_ODs.csv' CSV HEADER;
+
+WITH sub AS (
+	SELECT 
+		i,o,d,
+		degrees( ST_Azimuth( od1.geom::geography, od2.geom::geography ) ) AS az,
+		od1.geom::geography <-> od2.geom::geography AS meters,
+		ST_Area(od1.voronoi_geom::geography)/10^6 AS o_w, -- square km
+		ST_Area(od2.voronoi_geom::geography)/10^6 AS d_w  -- square km
+	FROM sampled_ods AS sod 
+	JOIN ttc_od AS od1 
+		ON o = od1.uid
+	JOIN ttc_od AS od2 
+		ON d = od2.uid
+)
+UPDATE sampled_ods SET
+	azimuth = az,
+	arc = meters,
+	weight = o_w * d_w
+FROM sub WHERE sub.i = sampled_ods.i;
+
+COPY (
+	SELECT * FROM sampled_ods ORDER BY i ASC
+) TO '/home/nate/routing/data/all_sampled_ODs_extra.csv' CSV HEADER;
+
+
+
+/*
+SELECT degrees( ST_Azimuth( 
+	ST_Point(0, 0)::geography, 
+	ST_Point(-1, 0)::geography 
+) )
+*/
