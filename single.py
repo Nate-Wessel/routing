d083036ab0059024349ca37de3d5e90ee49fc377
Nate-Wessel <bike756@gmail.com>
2019-02-19 15:26:01 -0500

    modified output formatting

--- single.py
+++ single.py
@@ -1,25 +1,18 @@
 # This is for getting detailed stats on a single OD, mainly for diagnostic tests
 from OD import OD
-import db
 
-oid = 222
-did = 208
+oid = 12
+did = 316
 od = OD(oid,did)
-
 print( od )
-print( 'realtime access:', od.access('realtime') )
-print( 'habitual-access:', od.access('habitual') )
-print( 'any access:', od.access('any') )
 
-# get the departures and arrivals for the top three trips
-# and put them in a CSV
+# get the departures and arrivals for the top trips and put them in a CSV
 
 # write all trips for the top itineraries to a file
-with open('data/db-trips.csv','w') as outfile:
-	outfile.write('depart,arrive,itinerary\n')
+with open('data/untracked/db-trips.csv','w') as outfile:
+	outfile.write('depart,arrive,itinerary')
 	for itin in od.alter_itins():
 		for trip in itin.get_trips():
-			outfile.write(str(trip.depart_ts)+','+str(trip.arrive_ts)+',"'+str(itin)+'"\n')
-
-#for trip in od.retro_trips:
-#	trip.verify()
+			outfile.write( '\n{},{},"{}"'.format(
+				trip.depart_ts, trip.arrive_ts, itin 
+			) )
