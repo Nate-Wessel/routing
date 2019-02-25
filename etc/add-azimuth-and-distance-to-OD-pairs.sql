e1467e378cd5bc21b712dbb8719d55e31d13d463
Nate-Wessel <bike756@gmail.com>
2019-02-25 18:52:19 -0500

    added manhattan distance to ODs

--- etc/add-azimuth-and-distance-to-OD-pairs.sql
+++ etc/add-azimuth-and-distance-to-OD-pairs.sql
@@ -5,7 +5,8 @@
 	azimuth real,
 	arc real,
 	o_area real,
-	d_area real
+	d_area real,
+	grid_dist real
 );
 COPY sampled_ods (i,o,d) FROM '/home/nate/routing/data/all_sampled_ODs.csv' CSV HEADER;
 
@@ -15,7 +16,8 @@ WITH sub AS (
 		degrees( ST_Azimuth( od1.geom::geography, od2.geom::geography ) ) AS az,
 		(od1.geom::geography <-> od2.geom::geography) / 1000 AS km,
 		ST_Area(od1.voronoi_geom::geography)/10^6 AS o_sqkm, -- square km
-		ST_Area(od2.voronoi_geom::geography)/10^6 AS d_sqkm  -- square km
+		ST_Area(od2.voronoi_geom::geography)/10^6 AS d_sqkm, -- square km
+		ST_Rotate( ST_MakeLine(od1.loc_geom,od2.loc_geom) ,radians(-17), od1.loc_geom ) AS v
 	FROM sampled_ods AS sod 
 	JOIN ttc_od AS od1 
 		ON o = od1.uid
@@ -26,12 +28,14 @@ UPDATE sampled_ods SET
 	azimuth = az,
 	arc = km,
 	o_area = o_sqkm,
-	d_area = d_sqkm 
+	d_area = d_sqkm,
+	grid_dist = (ABS(ST_X(ST_StartPoint(v))-ST_X(ST_EndPoint(v))) + ABS(ST_Y(ST_StartPoint(v))-ST_Y(ST_EndPoint(v))))/1000
 FROM sub WHERE sub.i = sampled_ods.i;
 
 COPY (
 	SELECT * FROM sampled_ods ORDER BY i ASC
-) TO '/home/nate/routing/data/all_sampled_ODs.csv' CSV HEADER;
+	--LIMIT 10000
+) TO '/home/nate/routing/data/sampled-ODs/all.csv' CSV HEADER;
 
 
 
