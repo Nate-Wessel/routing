fa19b696e3d0ce541865c70ab40215e9b18e3518
Nate-Wessel <bike756@gmail.com>
2019-02-26 13:28:59 -0500

    various

--- anal/entropy-vs-azimuth-plot.r
+++ anal/entropy-vs-azimuth-plot.r
@@ -1,20 +1,29 @@
-rad2deg <- function(rad) {(rad * 180) / (pi)}
-deg2rad <- function(deg) {(deg * pi) / (180)}
+deg2rad <- function(deg) {deg * pi / 180}
+rad2deg <- function(rad) {rad * 180 / pi}
 
 d = read.csv('~/routing/data/summary.csv')
+# set the weight
+gauss <- function(x){ bw = 5; return( exp(-(x**2 / ( 2 * bw**2 ) )) ) }
+d$area_weight = d$o_area * d$d_area
+
+d$dist_weight = 1
+
+d$gauss(d$arc)
+
 control_dens = density(
 	x=c(d$azimuth-360,d$azimuth,d$azimuth+360),
-	from=-180,to=180,n=516,bw=10
+	weights=rep(d$weight,3)/sum(d$weight*3),
+	from=-180,to=180,n=516,bw=8
 )
-var_dens = density(
-	x= c(d$azimuth-360,d$azimuth,d$azimuth+360),
-	weights=rep(d$retro_ent,3)/sum(d$retro_ent*3),
-	from=-180,to=180,n=516,bw=10
-)
+#var_dens = density(
+#	x= c(d$azimuth-360,d$azimuth,d$azimuth+360),
+#	weights=rep(d$weight,3)/sum(d$weight*3),
+#	from=-180,to=180,n=516,bw=8
+#)
 
-# and make a simpler data structure
-angle  = deg2rad(var_dens$x)
-radius = var_dens$y / control_dens$y / 3
+# convert to radians and change reference point
+angle  = deg2rad(-control_dens$x-90)
+radius = control_dens$y * 300
 # find points on a circle
 x = 0.5 + radius * cos(angle)
 y = 0.5 + radius * sin(angle)
@@ -26,12 +35,12 @@ lines(x,y)
 ew = deg2rad(17)    # east - west
 ns = deg2rad(17+90) # north - south
 lines(
-	c(0.5,(0.5+1*cos(ew))),
-	c(0.5,(0.5+1*sin(ew))),
-	col='red'
+	c( 0.5-cos(ew) , 0.5+cos(ew) ),
+	c( 0.5-sin(ew) , 0.5+sin(ew) ),
+	col='blue'
 )
 lines(
-	c(0.5,(0.5+1*cos(ns))),
-	c(0.5,(0.5+1*sin(ns))),
-	col='green'
+	c( 0.5-cos(ns), 0.5+cos(ns) ),
+	c( 0.5-sin(ns), 0.5+sin(ns) ),
+	col='blue'
 )
