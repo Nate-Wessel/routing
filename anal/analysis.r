90eaf6212e890b843a2e1693ec2f358b12537f27
Nate-Wessel <bike756@gmail.com>
2019-02-26 16:35:41 -0500

    moved OD reading to reparate module

--- anal/analysis.r
+++ anal/analysis.r
@@ -1,52 +1,7 @@
-#######################################
-# read in OD level data and assign weights to observations
-#######################################
-# read in the OD pairs
-periods = c('am-peak','midday','pm-peak','evening')
-# periods will be weighted evenly though they have different durations: 3,6,4,3 hours 
-for( period in periods ){
-	fname = paste0('~/routing/data/untracked/',period,'-od-stats.csv')
-	if( file.exists(fname) ){
-		print(paste('reading',fname))
-		if( ! exists('ods') ){
-			ods = read.csv(fname)
-			ods$period = period
-		}else{
-			d = read.csv(fname)
-			d$period = period
-			ods = rbind(ods,d)
-			remove(d)
-		}
-	}
-}
-# name the data, remove unecessary fluff
-#rownames(ods) = paste0(ods$o,'->',ods$d)
-ods$pair = factor(paste0(ods$o,'->',ods$d))
-ods$period = factor(ods$period)
-ods$o = ods$d = ods$i = ods$arc = NULL
-# assign weights based on areas
-ods$weight = ods$o_area * ods$d_area
-ods$weight = ods$weight / sum(ods$weight)
-# and weights based on length distributions
-sample_density = density( ods$grid, weights=ods$weight, from=0, to=47, n=516, bw=1 )
-obs = read.csv('~/routing/data/TTS/observed_trip_lengths.csv')
-observed_density = density( obs$grid_dist, weights=obs$trips/sum(obs$trips), from=0, to=47, n=516, bw=1 )
-cairo_pdf('~/Dropbox/diss/routing/paper/figures/weights-adjustment.pdf',width=6,height=3)
-	par(mar=c(4,0,2,0),family='serif')
-	plot(observed_density,main='Unweighted Sample vs. Observed Trip Lengths',
-		  col='red',xlab='Kilometers (Manhattan Distance)',
-		  bty='n',yaxt='n',ylab='')
-	lines(sample_density,col='grey',lty=2)
-	# adjust weights iteratively to mimic desired distribution
-	for(i in 1:15){
-		adj_factor = observed_density$y / sample_density$y
-		ods$weight = ods$weight * approx(x=sample_density$x,y=adj_factor,xout=ods$grid)$y
-		ods$weight = ods$weight / sum(ods$weight)
-		sample_density = density( ods$grid, weights=ods$weight, from=0, to=47, n=516, bw=1 )
-		lines(sample_density,col=rgb(0,0,1,alpha=0.1))
-	}
-dev.off()
-remove(i,adj_factor,observed_density,sample_density,obs)
+# read in OD level data (weighted)
+source('~/routing/anal/read-and-weight-ods.r')
+
+
 
 ###############################################
 # read in the db-estimated departure times data
@@ -62,7 +17,11 @@ t$delta_real = t$real - t$any
 # how often are the alternatives simply equivalent?
 sum((t$any==t$hab)*t$weight,na.rm=T)/sum(t$weight)
 sum((t$any==t$real)*t$weight,na.rm=T)/sum(t$weight)
-# 
+
+
+
+
+
 ################################
 # analyze stats per OD
 ################################
